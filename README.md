11c6489224942be164b0fe87e43193d49c62f504
Aboubakr06 <aboubakerelbouzidi@gmail.com>
2023-01-11 20:12:57 +0100

    commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Interactive rating component solution
+
+This is a solution to the [Interactive rating component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+
+
+### Screenshot
+
+-Mobile version-
+
+![photo](./design/mobile-design.jpg)
+![photo](./design/mobile-thank-you-state.jpg)
+
+ -Desktop version
+
+![photo](./design/desktop-design.jpg)
+![photo](./design/desktop-thank-you-state.jpg)
+
+### Links
+
+- Solution URL: ()
+- Live Site URL: (https://nimble-lollipop-d62801.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JavaScript
+
+## Author
+
+- Frontend Mentor - [@Aboubakr06](https://www.frontendmentor.io/profile/Aboubakr06)
+- Twitter - [@elbouzidi99](https://twitter.com/elbouzidi99)
+
---
+++ script.js
@@ -0,0 +1,119 @@
+let SubmitBtn = document.getElementById("submit-btn");
+let RateAgain = document.getElementById("rate-again")
+let TopContainer = document.querySelector(".top");
+let BottomContainer = document.querySelector(".bottom");
+
+let OneBtn = document.getElementById("1");
+let TwoBtn = document.getElementById("2");
+let ThreeBtn = document.getElementById("3");
+let FourBtn = document.getElementById("4");
+let FiveBtn = document.getElementById("5");
+
+let RatingVal = "";
+let selectedRat = document.getElementById("selected-rating")
+
+
+OneBtn.addEventListener("click", function() {
+    OneBtn.style.background = "hsl(217, 12%, 63%)";
+    OneBtn.style.color = "white"
+    
+    TwoBtn.style.background = "hsl(213, 19%, 15%)";
+    ThreeBtn.style.background = "hsl(213, 19%, 15%)";
+    FourBtn.style.background = "hsl(213, 19%, 15%)";
+    FiveBtn.style.background = "hsl(213, 19%, 15%)";
+
+    TwoBtn.style.color = "hsl(217, 12%, 63%)";
+    ThreeBtn.style.color = "hsl(217, 12%, 63%)";
+    FourBtn.style.color = "hsl(217, 12%, 63%)";
+    FiveBtn.style.color = "hsl(217, 12%, 63%)";
+    
+    RatingVal = 1
+    selectedRat.innerHTML = `You selected ${RatingVal} out of 5`
+})
+
+TwoBtn.addEventListener("click", function() {
+    TwoBtn.style.background = "hsl(217, 12%, 63%)";
+    TwoBtn.style.color = "white"
+
+    OneBtn.style.background = "hsl(213, 19%, 15%)";
+    ThreeBtn.style.background = "hsl(213, 19%, 15%)";
+    FourBtn.style.background = "hsl(213, 19%, 15%)";
+    FiveBtn.style.background = "hsl(213, 19%, 15%)";
+
+    OneBtn.style.color = "hsl(217, 12%, 63%)";
+    ThreeBtn.style.color = "hsl(217, 12%, 63%)";
+    FourBtn.style.color = "hsl(217, 12%, 63%)";
+    FiveBtn.style.color = "hsl(217, 12%, 63%)";
+
+    RatingVal = 2
+    selectedRat.innerHTML = `You selected ${RatingVal} out of 5`
+})
+
+ThreeBtn.addEventListener("click", function() {
+    ThreeBtn.style.background = "hsl(217, 12%, 63%)";
+    ThreeBtn.style.color = "white"
+
+    OneBtn.style.background = "hsl(213, 19%, 15%)";
+    TwoBtn.style.background = "hsl(213, 19%, 15%)";
+    FourBtn.style.background = "hsl(213, 19%, 15%)";
+    FiveBtn.style.background = "hsl(213, 19%, 15%)";
+
+    OneBtn.style.color = "hsl(217, 12%, 63%)";
+    TwoBtn.style.color = "hsl(217, 12%, 63%)";
+    FourBtn.style.color = "hsl(217, 12%, 63%)";
+    FiveBtn.style.color = "hsl(217, 12%, 63%)";
+
+    RatingVal = 3
+    selectedRat.innerHTML = `You selected ${RatingVal} out of 5`
+})
+
+FourBtn.addEventListener("click", function() {
+    FourBtn.style.background = "hsl(217, 12%, 63%)";
+    FourBtn.style.color = "white"
+    
+    OneBtn.style.background = "hsl(213, 19%, 15%)";
+    TwoBtn.style.background = "hsl(213, 19%, 15%)";
+    ThreeBtn.style.background = "hsl(213, 19%, 15%)";
+    FiveBtn.style.background = "hsl(213, 19%, 15%)";
+
+    OneBtn.style.color = "hsl(217, 12%, 63%)";
+    TwoBtn.style.color = "hsl(217, 12%, 63%)";
+    ThreeBtn.style.color = "hsl(217, 12%, 63%)";
+    FiveBtn.style.color = "hsl(217, 12%, 63%)";
+    
+    RatingVal = 4
+    selectedRat.innerHTML = `You selected ${RatingVal} out of 5`
+})
+
+FiveBtn.addEventListener("click", function() {
+    FiveBtn.style.background = "hsl(217, 12%, 63%)";
+    FiveBtn.style.color = "white"
+    
+    OneBtn.style.background = "hsl(213, 19%, 15%)";
+    TwoBtn.style.background = "hsl(213, 19%, 15%)";
+    ThreeBtn.style.background = "hsl(213, 19%, 15%)";
+    FourBtn.style.background = "hsl(213, 19%, 15%)";
+
+    OneBtn.style.color = "hsl(217, 12%, 63%)";
+    TwoBtn.style.color = "hsl(217, 12%, 63%)";
+    ThreeBtn.style.color = "hsl(217, 12%, 63%)";
+    FourBtn.style.color = "hsl(217, 12%, 63%)";
+    
+    RatingVal = 5
+    selectedRat.innerHTML = `You selected ${RatingVal} out of 5`
+})
+
+
+SubmitBtn.addEventListener("click", function () {
+    if (RatingVal > 0) {
+    TopContainer.style.display = "none";
+    BottomContainer.style.display = "grid";
+}
+
+});
+
+RateAgain.addEventListener("click", function () {
+    TopContainer.style.display = "grid";
+    BottomContainer.style.display = "none";
+
+});
